e2001e730018696ac9c1ad10f50f24384f82bb8f
Vivacity <vivacity_rex@outlook.com>
2023-05-25 10:48:15 -0400

    Added moment js

---
+++ src/script.js
@@ -0,0 +1,50 @@
+//Los Angeles
+setInterval(function () {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<em>]SSS[</em>] [<small>]A[</small>]"
+  );
+}, 1);
+
+//Paris
+function updateParisTime() {
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+updateParisTime();
+setInterval(updateParisTime, 1000);
+
+function updateTime() {
+  //Ottawa
+  let ottawaElement = document.querySelector("#ottawa");
+  let ottawaDateElement = ottawaElement.querySelector(".date");
+  let ottawaTimeElement = ottawaElement.querySelector(".time");
+  let ottawaTime = moment().tz("Canada/Ottawa");
+
+  ottawaDateElement.innerHTML = ottawaTime.format("MMMM Do YYYY");
+  ottawaTimeElement.innerHTML = ottawaTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
